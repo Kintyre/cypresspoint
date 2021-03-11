7c479da2240063dddeb6d204012ce12cd02f476d
Lowell Alleman <lowell@kintyre.co>
2021-03-11 16:50:14 -0500

    Import splunk_dot_notation and friends
    - Imported splunk_dot_notation and supporting functions from cypress_ta_spl
    - Created unit tests for splunk_dot_notation (spath-like) functionality.

---
+++ cypresspoint/spath.py
@@ -0,0 +1,56 @@
+import re
+
+
+
+def sanitize_fieldname(field):
+    clean = re.sub(r'[^A-Za-z0-9_.{}\[\]-]', "_", field)
+    # Remove leading/trailing underscores
+    clean = clean.strip("_")
+    return clean
+
+
+def dict_to_splunk_fields(obj, prefix=()):
+    """
+    Input:  Object   (dict, list, str/int/float)
+    Output:  [  ( (name,name), value) ]
+
+    Convention:  Arrays suffixed with "{}"
+    """
+    output = []
+    if isinstance(obj, dict):
+        for key, value in obj.items():
+            key = sanitize_fieldname(key)
+            output.extend(dict_to_splunk_fields(value, prefix=prefix+(key,)))
+    elif isinstance(obj, (list, list)):
+        if prefix:
+            prefix = prefix[:-1] + (prefix[-1] + "{}",)
+            for item in obj:
+                output.extend(dict_to_splunk_fields(item, prefix=prefix))
+    elif isinstance(obj, bool):
+        output.append((prefix, "true" if obj else "false"))
+    elif isinstance(obj, (str, int, float)) or obj is None:
+        output.append((prefix, obj))
+    else:
+        raise TypeError("Unsupported datatype {}".format(type(obj)))
+    return output
+
+
+def splunk_dot_notation(obj):
+    """
+    Convert json object (python dictionary) into a list of fields as Splunk does by default.
+    Think of this as the same as calling Splunk's "spath" SPL command.
+    """
+    d = {}
+    if not isinstance(obj, dict):
+        raise ValueError("Expected obj to be a dictionary, received {}".format(type(obj)))
+    for field_pair, value in dict_to_splunk_fields(obj):
+        field_name = ".".join(field_pair)
+        if field_name in d:
+            if not isinstance(d[field_name], list):
+                d[field_name] = [d[field_name]]
+            d[field_name].append(value)
+        else:
+            d[field_name] = value
+    return d
+
+
---
+++ tests/test_spath.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from cypresspoint.spath import splunk_dot_notation
+
+
+class TestSpath(unittest.TestCase):
+
+    def test_simple_object(self):
+        """Test something."""
+        test = {
+            "a": 1,
+            "b": 2,
+        }
+        output = splunk_dot_notation(test)
+        self.assertEqual(output, test)
+
+    def test_invalid_field_name(self):
+        f = splunk_dot_notation
+        self.assertEqual(f({"_a": 1, "b": 2, }), {"a": 1, "b": 2})
+        self.assertEqual(f({"$junk": "apple"}), {"junk": "apple"})
+        self.assertEqual(f({"ca$h": "apple"}), {"ca_h": "apple"})
+
+        # Make sure values are unaffected
+        self.assertEqual(f({"good": "$$!CRAZY#$"}), {"good": "$$!CRAZY#$"})
+
+    def test_invalid_values(self):
+        f = splunk_dot_notation
+
+        with self.assertRaises(TypeError):
+            # Sometimes this fails?????
+            splunk_dot_notation({"field": tuple()})
+
+        with self.assertRaises(TypeError):
+            f({"field": set()})
+        with self.assertRaises(TypeError):
+            f({"field": object()})
+        with self.assertRaises(TypeError):
+            f({"field": object})
+
+    def test_bad_input(self):
+        with self.assertRaises(ValueError):
+            splunk_dot_notation([{"a":1},{"a":2}])
+
+    '''
+    #@unittest.expectedFailure
+        def test_things_that_i_cant_explain(self):
+            # Why is a tuple allowed here?   (not allowed explicitly in code)
+            # Q:  caught by isinstance(, list) ?
+    '''
+
+    def test_unicode(self):
+        # Reproduce bug found between Python 2 vs 3
+        from six import text_type
+        splunk_dot_notation({"a": text_type("value")})
+
+    def test_bool_to_text(self):
+        d = {
+            "t": True,
+            "f": False
+        }
+        self.assertEqual(splunk_dot_notation(d), {"t": "true", "f":"false"})
+
+    def test_nested01(self):
+        d = {
+            "object": {"a": 1, "x": "two"},
+            "array": ["1", "2", "3"]
+        }
+        e = {
+            "object.a": 1,
+            "object.x": "two",
+            "array{}": ["1", "2", "3"]
+        }
+        output = splunk_dot_notation(d)
+        self.assertEqual(output, e)
+
+        d["array_of_objs"] = [
+            {"a": 1, "z": 1},
+            {"a": 2, "z": 2},
+        ]
+        e["array_of_objs{}.a"] = [1, 2]
+        e["array_of_objs{}.z"] = [1, 2]
+        output = splunk_dot_notation(d)
+        self.assertEqual(output, e)
+
+    def test_nested02(self):
+        d = {
+            "server": {
+                "obj": [1, 2, 3]
+            },
+            "blah": {
+                "a": 1,
+                "b": 2,
+                "c": [
+                    {"a": 99},
+                    {"a": 98}
+                ],
+            }
+        }
+        e = {
+            "server.obj{}": [1, 2, 3],
+            "blah.a": 1,
+            "blah.b": 2,
+            "blah.c{}.a": [99, 98],
+        }
+        output = splunk_dot_notation(d)
+        self.assertDictEqual(output, e)
