4b5882e9d28b00ddc1c1a93c6c5b3e32c6bf9e24
Lowell Alleman <lowell@kintyre.co>
2021-06-07 17:02:42 -0400

    Bump version: 0.5.0 → 0.5.1

--- cypresspoint/__init__.py
+++ cypresspoint/__init__.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 
 __author__ = """Lowell Alleman"""
 __email__ = 'lowell@kintyre.co'
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 
 
 def setup_logging(log_file, debug=False):
