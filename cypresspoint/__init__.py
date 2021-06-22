8e5069ce253a7afbd8e6b48079424be362fb166d
Lowell Alleman <lowell@kintyre.co>
2021-06-22 18:11:05 -0400

    Support setup_logging() with size-based rotation
    - Support time or size based log rotation.
    - Default time-based rotation to 'midnight' instead of days, which was both
      incorrect and only helpful for very long running processes.
    - Allow custom formatter to be given; helpful when thread id should be
      captured, for example.

--- cypresspoint/__init__.py
+++ cypresspoint/__init__.py
@@ -9,19 +9,27 @@ __email__ = 'lowell@kintyre.co'
 __version__ = '0.6.1'
 
 
-def setup_logging(log_file, debug=False):
+def setup_logging(log_file, debug=False, formatter=None, when="midnight",
+                  max_size_mb=None, backup_count=10):
     from logging import DEBUG, INFO, Formatter, getLogger
-    from logging.handlers import TimedRotatingFileHandler
+    from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
     log = getLogger()
     if debug:
         log_level = DEBUG
     else:
         log_level = INFO
     log.setLevel(log_level)
-    logfile = TimedRotatingFileHandler(log_file, "d", 7, backupCount=10, encoding='utf-8')
+
+    if max_size_mb:
+        logfile = RotatingFileHandler(log_file, maxBytes=max_size_mb * 1048576,
+                                      backupCount=backup_count, encoding='utf-8')
+    else:
+        logfile = TimedRotatingFileHandler(log_file, when,
+                                           backupCount=backup_count, encoding='utf-8')
     logfile.setLevel(log_level)
     # setup log file formatting:
-    logfile.setFormatter(
-        Formatter('%(asctime)s [%(process)d] %(levelname)-8s %(name)s:  %(message)s'))
+    if formatter is None:
+        formatter = Formatter('%(asctime)s [%(process)d] %(levelname)-8s %(name)s:  %(message)s')
+    logfile.setFormatter(formatter)
     log.addHandler(logfile)
     return log
