9afbba329e6a6935c4be08e9a6db26566761e6d9
Lowell Alleman <lowell.alleman@cdillc.com>
2023-02-27 21:22:23 -0500

    Bump version: 0.7.3 → 0.7.4

--- cypresspoint/__init__.py
+++ cypresspoint/__init__.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 
 __author__ = """Lowell Alleman"""
 __email__ = 'lowell@kintyre.co'
-__version__ = '0.7.3'
+__version__ = '0.7.4'
 
 
 def setup_logging(log_file, debug=False, formatter=None, when="midnight",
--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Kintyre/cypresspoint',
-    version='0.7.3',
+    version='0.7.4',
     zip_safe=False,
 )
