5cd7c9e26bbc1a1c4147fb5ee077ccdf21995945
Lowell Alleman <lowell.alleman@cdillc.com>
2023-03-02 17:03:23 -0500

    Bump version: 0.7.4 → 0.8.0

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Kintyre/cypresspoint',
-    version='0.7.4',
+    version='0.8.0',
     zip_safe=False,
 )
